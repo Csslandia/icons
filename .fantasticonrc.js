dcd2cc1225d8978073da44d6e1e9c1423344acf9
jacobheap <jacobheap0@gmail.com>
2023-05-21 19:18:20 +0200

    init

---
+++ .fantasticonrc.js
@@ -0,0 +1,9 @@
+export default {
+    inputDir: "./svg",
+    outputDir: "./font",
+    fontTypes: ["woff2"],
+    assetTypes: ["css"],
+    name: "Csslandia-icons",
+    tag: '',
+    prefix: 'i'
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+import { generateFonts } from "fantasticon";
+
+import config from "./.fantasticonrc.js";
+
+generateFonts(config);
